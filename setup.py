0c433be58be73ddfba323e1c8b04681e4dae859f
Geoff Kendal <gkendal@williamhill.co.uk>
2021-02-08 20:13:48 +0000

    Pypi bits

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Creates Star graphics mode raster images for use with Star TSP series thermal printers',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    install_requires=['numpy'],
+    install_requires=['Pillow'],
     url='https://github.com/geftactics/python-StarTSPImage',
     download_url = 'https://github.com/geftactics/python-StarTSPImage/archive/0.1.0.tar.gz'
     author='Geoff Kendal',
